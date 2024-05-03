d087190932b8a207f6a9d4ddd8a784bccdc2ef20
Angie Rodriguez <angrodri902@gmail.com>
2024-05-03 05:58:33 -0700

    styling tweaks and add html sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './vite.svg'
-import './App.css'
+// import { useState } from 'react';
+import profilePic from "./assets/profile.png"
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://angierodriguez.dev" target="_blank">
+          <img src={profilePic} className="logo" alt="Profile Pic" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="section__main">
+        <h1>Angie Rodriguez</h1>
+        <p className='profession'>Frontend Developer</p>
+        <small>angierodriguez.dev</small>
+        <div className="button-container">
+          <button className="button">Email</button>
+          <button className="button">LinkedIn</button>
+        </div>
+        <section className='section-copy'>
+          <h2>About</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil cumque eius consequatur labore dignissimos voluptas, excepturi quae est, provident, sint pariatur magnam soluta minus! Expedita ut eveniet corporis error dolore.</p>
+        </section>
+        <section className='section-copy'>
+          <h2>Interest</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nobis magnam dicta ipsum, nam repellendus! Enim necessitatibus beatae explicabo perferendis sint non est, magni quo voluptas facilis unde optio quod.</p>
+        </section>
+      </main>
+      <footer className="section__footer">
+        <p>Footer copy</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
